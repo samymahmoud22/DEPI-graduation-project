01f4adc2d4570f283ae548c267eba14722fcdb41
samymahmoud22 <samymahmoud2060@gmail.com>
2026-04-25 11:08:43 +0300

    Replace project with visionmate

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.visiontest"
+    namespace = "com.example.visionmate"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.visiontest"
+        applicationId = "com.example.visionmate"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,17 +8,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:visiontest/main.dart';
+import 'package:visionmate/main.dart';
 
 void main() {
-  testWidgets('App loads smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const VisionMateApp());
+    await tester.pumpWidget(const MyApp());
 
-    // Wait for the splash screen to render
-    await tester.pumpAndSettle();
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-    // Verify that the title is present.
-    expect(find.text('Vision Mate'), findsWidgets);
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
